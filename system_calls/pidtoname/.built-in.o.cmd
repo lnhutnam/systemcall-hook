673c49d589a87362f168ccbb8e8e7d6611f0b680
lenhutnam298 <18120061@student.hcmus.edu.vn>
2020-12-29 01:46:34 -0800

    syscalls pidtoname, pnametoid

---
+++ system_calls/pidtoname/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_pidtoname/built-in.o :=  ld -m elf_i386   -r -o pidtoname/built-in.o pidtoname/pidtoname.o 
---
+++ system_calls/pnametoid/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_pnametoid/built-in.o :=  ld -m elf_i386   -r -o pnametoid/built-in.o pnametoid/pnametoid.o 
