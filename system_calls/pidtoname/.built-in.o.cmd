ea1ad48a49fc6ed88bb4714b64edac4efb963f0c
lenhutnam298 <18120061@student.hcmus.edu.vn>
2020-12-29 01:52:53 -0800

    Revert "syscalls pidtoname, pnametoid"
    This reverts commit 673c49d589a87362f168ccbb8e8e7d6611f0b680.

--- system_calls/pidtoname/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_pidtoname/built-in.o :=  ld -m elf_i386   -r -o pidtoname/built-in.o pidtoname/pidtoname.o 
--- system_calls/pnametoid/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_pnametoid/built-in.o :=  ld -m elf_i386   -r -o pnametoid/built-in.o pnametoid/pnametoid.o 
